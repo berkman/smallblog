1afb88d90c32f600d002c2da3950db1956833c44
Mike Berkman <mberkman@cisco.com>
2016-01-24 15:51:58 -0500

    Fixed include.php to support postgres and removed some old files

--- include/header.php
+++ include/header.php
@@ -2,9 +2,9 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-  <title>bongit<? if ($title != ""){ echo " - " . $title; } ?></title>
+  <title>simple blog<? if ($title != ""){ echo " - " . $title; } ?></title>
   <script language="JavaScript" src="include/script.js" type="text/javascript"></script>
-  <link href="<? echo "http://" . $_SERVER['HTTP_HOST'] . dirname($SERVER['PHP_SELF']) . "/include/style.css"; ?>" type="text/css" rel="stylesheet" />
+  <link href="/include/style.css" type="text/css" rel="stylesheet" />
 </head>
 <body>
-<h1>bongit (logo needed)</h1>
+<h1>simple blog</h1>
--- include/include.php
+++ include/include.php
@@ -1,12 +1,24 @@
 <?
 
+// MYSQL
+/*
 $link = mysql_connect('connection', 'user', 'password');
 $database = "bongit";
 
 if (!$link) {
-   die('Could not connect: ' . mysql_error());
+  die('Could not connect: ' . mysql_error());
 }
 
 @mysql_select_db($database) or die ("Unable to connect to the database.");
+*/
+
+// POSTGRES
+$dbname =     $_ENV["POSTGRES_DBNAME"];
+$host =       $_ENV["POSTGRES_HOST"];
+$port =       $_ENV["POSTGRES_PORT"];
+$user =       $_ENV["POSTGRES_USERNAME"];
+$password =   $_ENV["POSTGRES_PASSWORD"];
+
+$link = pg_connect("$dbname $host $port $user $password sslmode=require");
 
 ?>
